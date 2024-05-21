87fc5bf8ec91189d1278e91044e8533d58f6941d
Jorge-Marcelo <jorge3258@hotmail.com>
2024-05-21 19:51:32 -0300

    Primeiro commit

---
+++ Funcionario.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algoritmo_357
+{
+ internal class Funcionario
+ {
+public string Nome { get; set; }
+public double Salario { get; set; }
+
+public Funcionario(string nome, double salario){
+   Nome = nome;
+   Salario = salario;
+   AplicarAumento();
+  }
+private void AplicarAumento(){
+   Salario = Salario * 1.08;
+  }
+
+ }
+}
---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algoritmo_357
+{
+ internal class Program
+ {
+  static void Main(string[] args)
+  {
+
+   Funcionario[] funcionarios = new Funcionario[5];
+   
+
+   for (int iterador = 0; iterador < 5; iterador++){
+    Console.Write($"Digite o {iterador + 1}° nome: ");
+    string nome = Console.ReadLine();
+
+    Console.WriteLine("-------------------------");
+    Console.Write("Entre com os salarios: ");
+   double salario = double.Parse(Console.ReadLine());
+    Console.WriteLine("-------------------------");
+
+
+    funcionarios[iterador] = new Funcionario(nome, salario);
+   }
+
+   Console.WriteLine("Relação dos Funcionarios");
+for(int iterador = 0; iterador < 5; iterador++){
+    Console.WriteLine($"{funcionarios[iterador].Nome} - {funcionarios[iterador].Salario}");
+   }
+  }
+
+
+ }
+}
